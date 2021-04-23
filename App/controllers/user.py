5da19e3655b39be4b1891b7bc6c120e3c7106968
Shakell25 <shakell25@live.com>
2021-04-23 02:27:39 +0000

    first commit

--- App/controllers/user.py
+++ App/controllers/user.py
@@ -2,6 +2,13 @@ from flask import redirect, render_template, request, session, url_for
 
 from App.models import ( User )
 
+def get_users_jason():
+    users = User.query.all()
+    if not users:
+        return jsonify([])
+    json = [user.toDict() for user in users]
+    return json
+
 def create_user(firstname, lastname, uwi_id, email, gender, dob):
     # newuser = use()
     return 'new user'
--- App/views/user.py
+++ App/views/user.py
@@ -2,7 +2,7 @@ from flask import Blueprint, redirect, render_template, request, jsonify, send_f
 
 user_views = Blueprint('user_views', __name__, template_folder='../templates')
 
-from App.models import User
+from App.controllers import ( get_users, get_users_jason, create_user )
 
 @user_views.route('/users', methods=['GET'])
 def get_user_page():
